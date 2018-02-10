26cdec5a8803bfca4b60619a938668a9ddbdf734
David Dassau <todieahero@hotmail.com>
2018-02-10 12:53:26 -0600

    completed roman numeral I

---
+++ RomanNumeralConverter.Tests/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using RomanNumeralConverter.App;
+
+namespace RomanNumeralConverter.Tests
+{
+    [TestFixture]
+    public class NumberConverterTests
+    {
+        [Test]
+        public void Entering_a_number_results_in_that_number_being_converted_to_a_roman_numeral()
+        {
+            // Arrange
+            int numberOne = 1;
+            var numberConverter = new NumberConverter();
+
+            // Act
+            var result = numberConverter.Parse(numberOne);
+
+            // Assert
+            Assert.AreEqual("I", result);
+        }
+    }
+}
